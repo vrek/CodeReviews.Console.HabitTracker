ba939a85f9602ea0abe0388c0a12208a692fcb4a
Raymond Koons <ray.koons@gmail.com>
2023-05-19 13:27:09 -0500

    initial setup

---
+++ HabitTracker.vrek/db_connect.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker.vrek
+{
+    internal class Database
+    {
+        public void CreateDatabase(string datebaseName)
+        {
+
+        }
+        public void TestDatabase(string databaseName)
+        {
+            try
+            {
+                using (SqliteConnection connection = new SqliteConnection())
+                {
+                    connection.Open();
+                }
+            }
+        }
+    }
+
+}
